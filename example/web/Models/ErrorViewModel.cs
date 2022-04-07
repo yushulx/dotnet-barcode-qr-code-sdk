f2ecfa2a7627e1f3faa1651adf052ba1176775e2
yushulx <lingxiao1002@gmail.com>
2022-04-07 10:17:42 +0800

    Added asp.net MVC example

---
+++ example/web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MvcBarcodeQRCode.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
