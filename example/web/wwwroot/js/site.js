bb0d8300b7289154b966933fa462f3f5b1b76d16
yushulx <lingxiao1002@gmail.com>
2022-04-07 14:57:19 +0800

    Updated

--- example/web/wwwroot/js/site.js
+++ example/web/wwwroot/js/site.js
@@ -2,3 +2,33 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.getElementById("file").addEventListener("change", function () {
+    let file = this.files[0];
+    let image = document.getElementById('image');
+    let reader = new FileReader();
+
+    reader.addEventListener("load", function () {
+        image.src = reader.result;
+    }, false);
+
+    if (file) {
+        reader.readAsDataURL(file);
+    }
+});
+
+function upload() {
+    const host = location.hostname
+    const protocol = location.protocol
+    const uploadPath = '/upload'
+    let xhr = new XMLHttpRequest();
+    let formData = new FormData();
+    let file = document.getElementById('file').files[0];
+    formData.append('barcodeImage', file, file.name);
+    xhr.open('POST', uploadPath, true);
+    xhr.send(formData);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            document.getElementById("results").innerHTML = "Detection Results: " + xhr.responseText;
+        }
+    }
+}
