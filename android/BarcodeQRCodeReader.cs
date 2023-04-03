9d4ee156bfe42d72361b85d5a373d515909dceb8
yushulx <lingxiao1002@gmail.com>
2023-04-03 13:58:57 +0800

    Updated

--- android/BarcodeQRCodeReader.cs
+++ android/BarcodeQRCodeReader.cs
@@ -4,13 +4,16 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using Android.Hardware.Lights;
 using Com.Dynamsoft.Dbr;
+using Java.Nio;
+using Java.Nio.FileNio.Attributes;
 
 namespace Dynamsoft
 {
     public class BarcodeQRCodeReader
     {
-        private BarcodeReader reader;
+        private BarcodeReader? reader;
 
         public class Result
         {
@@ -18,6 +21,48 @@ namespace Dynamsoft
             public int[]? Points { get; set; }
             public string? Format1 { get; set; }
             public string? Format2 { get; set; }
+        }
+
+        public enum ImagePixelFormat
+        {
+            /**0:Black, 1:White */
+            IPF_BINARY,
+
+            /**0:White, 1:Black */
+            IPF_BINARYINVERTED,
+
+            /**8bit gray */
+            IPF_GRAYSCALED,
+
+            /**NV21 */
+            IPF_NV21,
+
+            /**16bit with RGB channel order stored in memory from high to low address*/
+            IPF_RGB_565,
+
+            /**16bit with RGB channel order stored in memory from high to low address*/
+            IPF_RGB_555,
+
+            /**24bit with RGB channel order stored in memory from high to low address*/
+            IPF_RGB_888,
+
+            /**32bit with ARGB channel order stored in memory from high to low address*/
+            IPF_ARGB_8888,
+
+            /**48bit with RGB channel order stored in memory from high to low address*/
+            IPF_RGB_161616,
+
+            /**64bit with ARGB channel order stored in memory from high to low address*/
+            IPF_ARGB_16161616,
+
+            /**32bit with ABGR channel order stored in memory from high to low address*/
+            IPF_ABGR_8888,
+
+            /**64bit with ABGR channel order stored in memory from high to low address*/
+            IPF_ABGR_16161616,
+
+            /**24bit with BGR channel order stored in memory from high to low address*/
+            IPF_BGR_888
         }
 
         public class DBRLicenseVerificationListener : Java.Lang.Object, IDBRLicenseVerificationListener
@@ -48,17 +93,70 @@ namespace Dynamsoft
 
         ~BarcodeQRCodeReader()
         {
-            
+            Destroy();
         }
 
         public void Destroy()
         {
-            
+            reader = null;
         }
 
         public static string? GetVersionInfo()
         {
             return BarcodeReader.Version;
+        }
+
+        private Result[]? OutputResults(TextResult[]? results)
+        {
+            Result[]? output = null;
+            if (results != null && results.Length > 0)
+            {
+                output = new Result[results.Length];
+                for (int i = 0; i < results.Length; ++i)
+                {
+                    TextResult tmp = results[i];
+                    Result r = output[i];
+                    r.Text = tmp.BarcodeText;
+                    r.Format1 = tmp.BarcodeFormatString;
+                    r.Format2 = tmp.BarcodeFormatString2;
+                    IList<Point> points = tmp.LocalizationResult.ResultPoints;
+                    r.Points = new int[8] { points[0].X, points[0].Y, points[1].X, points[1].Y, points[2].X, points[2].Y, points[3].X, points[3].Y };
+                }
+            }
+
+            return output;
+        }
+
+        public Result[]? DecodeFile(string filename)
+        {
+            if (reader == null) { return null; }
+
+            TextResult[]? results = reader.DecodeFile(filename);
+            return OutputResults(results);
+        }
+
+        public Result[]? DecodeBuffer(byte[] buffer, int width, int height, int stride, ImagePixelFormat format)
+        {
+            if (reader == null) { return null; }
+
+            TextResult[]? results = reader.DecodeBuffer(buffer, width, height, stride, (int)format);
+
+            return OutputResults(results);
+        }
+
+        public Result[]? DecodeBase64(string base64string)
+        {
+            if (reader == null) { return null; }
+            TextResult[]? results = reader.DecodeBase64String(base64string);
+
+            return OutputResults(results);
+        }
+
+        public void SetParameters(string parameters)
+        {
+            if (reader == null) { return; }
+
+            reader.InitRuntimeSettingsWithString(parameters, EnumConflictMode.CmOverwrite);
         }
     }
 }
