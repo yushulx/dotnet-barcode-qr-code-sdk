afd653180d6aa2d024f2fd268712a4297f8db400
yushulx <lingxiao1002@gmail.com>
2023-04-03 16:42:31 +0800

    Updated

---
+++ example/maui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace BarcodeQRCode;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
