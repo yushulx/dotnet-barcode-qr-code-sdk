097439f13c94eb9ac3fe418a5072057e58a3cb4f
Xiao Ling <lingxiao1002@gmail.com>
2023-04-03 22:50:39 +0800

    Updated for ios

--- ios/BarcodeQRCodeReader.cs
+++ ios/BarcodeQRCodeReader.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Com.Dynamsoft.Dbr;
+
+using com.Dynamsoft.Dbr;
 
 namespace Dynamsoft
 {
     public class BarcodeQRCodeReader
     {
-        private BarcodeReader? reader;
+       
+        private DynamsoftBarcodeReader? reader;
 
         public class Result
         {
@@ -61,25 +63,25 @@ namespace Dynamsoft
             IPF_BGR_888
         }
 
-        public class DBRLicenseVerificationListener : Java.Lang.Object, IDBRLicenseVerificationListener
-        {
-            public void DBRLicenseVerificationCallback(bool isSuccess, Java.Lang.Exception error)
-            {
-                if (!isSuccess)
-                {
-                    System.Console.WriteLine(error.Message);
-                }
-            }
-        }
+        //public class DBRLicenseVerificationListener : Java.Lang.Object, IDBRLicenseVerificationListener
+        //{
+        //    public void DBRLicenseVerificationCallback(bool isSuccess, Java.Lang.Exception error)
+        //    {
+        //        if (!isSuccess)
+        //        {
+        //            System.Console.WriteLine(error.Message);
+        //        }
+        //    }
+        //}
 
         public static void InitLicense(string license)
         {
-            BarcodeReader.InitLicense(license, new DBRLicenseVerificationListener());
+            //BarcodeReader.InitLicense(license, new DBRLicenseVerificationListener());
         }
 
         private BarcodeQRCodeReader()
         {
-            reader = new BarcodeReader();
+            reader = new DynamsoftBarcodeReader();
         }
 
         public static BarcodeQRCodeReader Create()
@@ -99,66 +101,66 @@ namespace Dynamsoft
 
         public static string? GetVersionInfo()
         {
-            return BarcodeReader.Version;
+            return DynamsoftBarcodeReader.Version;
         }
 
-        private Result[]? OutputResults(TextResult[]? results)
-        {
-            Result[]? output = null;
-            if (results != null && results.Length > 0)
-            {
-                output = new Result[results.Length];
-                for (int i = 0; i < results.Length; ++i)
-                {
-                    TextResult tmp = results[i];
-                    Result r = new Result();
-                    output[i] = r;
-                    r.Text = tmp.BarcodeText;
-                    r.Format1 = tmp.BarcodeFormatString;
-                    r.Format2 = tmp.BarcodeFormatString;
-                    if (tmp.LocalizationResult != null && tmp.LocalizationResult.ResultPoints != null)
-                    {
-                        IList<Point> points = tmp.LocalizationResult.ResultPoints;
-                        r.Points = new int[8] { points[0].X, points[0].Y, points[1].X, points[1].Y, points[2].X, points[2].Y, points[3].X, points[3].Y };
-                    }
-                    else
-                        r.Points = null;
-                }
-            }
+        //private Result[]? OutputResults(TextResult[]? results)
+        //{
+        //    Result[]? output = null;
+        //    if (results != null && results.Length > 0)
+        //    {
+        //        output = new Result[results.Length];
+        //        for (int i = 0; i < results.Length; ++i)
+        //        {
+        //            TextResult tmp = results[i];
+        //            Result r = new Result();
+        //            output[i] = r;
+        //            r.Text = tmp.BarcodeText;
+        //            r.Format1 = tmp.BarcodeFormatString;
+        //            r.Format2 = tmp.BarcodeFormatString;
+        //            if (tmp.LocalizationResult != null && tmp.LocalizationResult.ResultPoints != null)
+        //            {
+        //                IList<Point> points = tmp.LocalizationResult.ResultPoints;
+        //                r.Points = new int[8] { points[0].X, points[0].Y, points[1].X, points[1].Y, points[2].X, points[2].Y, points[3].X, points[3].Y };
+        //            }
+        //            else
+        //                r.Points = null;
+        //        }
+        //    }
 
-            return output;
-        }
+        //    return output;
+        //}
 
-        public Result[]? DecodeFile(string filename)
-        {
-            if (reader == null) { return null; }
+        //public Result[]? DecodeFile(string filename)
+        //{
+        //    if (reader == null) { return null; }
 
-            TextResult[]? results = reader.DecodeFile(filename);
-            return OutputResults(results);
-        }
+        //    TextResult[]? results = reader.DecodeFile(filename);
+        //    return OutputResults(results);
+        //}
 
-        public Result[]? DecodeBuffer(byte[] buffer, int width, int height, int stride, ImagePixelFormat format)
-        {
-            if (reader == null) { return null; }
+        //public Result[]? DecodeBuffer(byte[] buffer, int width, int height, int stride, ImagePixelFormat format)
+        //{
+        //    if (reader == null) { return null; }
 
-            TextResult[]? results = reader.DecodeBuffer(buffer, width, height, stride, (int)format);
+        //    TextResult[]? results = reader.DecodeBuffer(buffer, width, height, stride, (int)format);
 
-            return OutputResults(results);
-        }
+        //    return OutputResults(results);
+        //}
 
-        public Result[]? DecodeBase64(string base64string)
-        {
-            if (reader == null) { return null; }
-            TextResult[]? results = reader.DecodeBase64String(base64string);
+        //public Result[]? DecodeBase64(string base64string)
+        //{
+        //    if (reader == null) { return null; }
+        //    TextResult[]? results = reader.DecodeBase64String(base64string);
 
-            return OutputResults(results);
-        }
+        //    return OutputResults(results);
+        //}
 
-        public void SetParameters(string parameters)
-        {
-            if (reader == null) { return; }
+        //public void SetParameters(string parameters)
+        //{
+        //    if (reader == null) { return; }
 
-            reader.InitRuntimeSettingsWithString(parameters, EnumConflictMode.CmOverwrite);
-        }
+        //    reader.InitRuntimeSettingsWithString(parameters, EnumConflictMode.CmOverwrite);
+        //}
     }
 }
